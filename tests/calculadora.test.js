ce968435fb3de946ff7fd767ba877909dbb0f49d
Thalys Row <123907051+ThalysRow@users.noreply.github.com>
2023-12-22 21:18:17 +0000

    add tests

---
+++ tests/calculadora.test.js
@@ -0,0 +1,16 @@
+const { somar } = require("../models/calculadora.js");
+
+test("somar 2 + 2 deve retornar 4", () => {
+  const resultado = somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("soma de 100 + 5 deve retornar 105", () => {
+  const resultado = somar(100, 5);
+  expect(resultado).toBe(105);
+});
+
+test("somar string + 100 deve retornar erro", () => {
+  const resultado = somar("string", 100);
+  expect(resultado).toBe("Error");
+});
