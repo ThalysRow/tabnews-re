9b486763d94985f3ea382db6e7a9a28995019f0e
Thalys Row <123907051+ThalysRow@users.noreply.github.com>
2023-12-23 22:30:57 +0000

    add ne test

--- models/calculadora.js
+++ models/calculadora.js
@@ -2,6 +2,9 @@ function somar(num1, num2) {
   if (typeof num1 !== "number") {
     return "Error";
   }
+  if (typeof num1 !== "number" && typeof num2 !== "number") {
+    return "Error";
+  }
 
   return num1 + num2;
 }
